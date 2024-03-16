aa5ec2d96d7495d6e6244d348ba31e8b106dad17
Bethan McKenzie <bethanmckenzie@gmail.com>
2024-03-16 14:22:22 +0000

    Continued setting up project, installed whitenoise and cloudinary, made initial blog model tbc

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    published_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=250)
+    image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    keywords = models.CharField(max_length=200, blank=True)
