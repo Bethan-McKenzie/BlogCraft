441f84198b66964c3efe01e55b57f52151623554
Bethan McKenzie <bethanmckenzie@gmail.com>
2024-03-18 22:28:26 +0000

    Added user model and related name to blog model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-
 
 class blog(models.Model):
     title = models.CharField(max_length=100)
@@ -10,5 +10,5 @@ class blog(models.Model):
     image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     keywords = models.CharField(max_length=200, blank=True)
-    likes = models.ManyToManyField(blank=True)
-    comments = models.ManyToManyField(blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    comments = models.ManyToManyField(User, related_name='blog_comments', blank=True)
