9588c7cb2b53be1e64eb7592cf9840d8c32d1c57
Bethan McKenzie <bethanmckenzie@gmail.com>
2024-03-21 15:01:34 +0000

    Fixed problem with comments not showing

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,9 @@ def my_profile(request):
 
 def detail(request, slug):
     post = get_object_or_404(blog, slug=slug)
+    comments = post.blog_comments.all()
+    print("I am comments", comments)
+
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -30,4 +33,4 @@ def detail(request, slug):
     else:
         form = CommentForm()
 
-    return render(request, 'blog/detail.html', {'post': post, 'form': form})
+    return render(request, 'blog/detail.html', {'post': post, 'form': form, 'comments': comments})
