a80a4a614f3328f52ce7aff16fca6a14cdcc36f6
Bethan McKenzie <bethanmckenzie@gmail.com>
2024-03-29 08:50:56 +0000

    Fixed issue with being unable to edit profile

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<slug:slug>/', views.detail, name='detail'),
     path('create_post/', views.create_post, name='create_post'),
     path('my_profile/', views.my_profile, name='my_profile'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('my_profile/edit_profile/', views.edit_profile, name='edit_profile'),
 ]
